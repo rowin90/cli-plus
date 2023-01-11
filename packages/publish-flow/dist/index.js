ddbf6b22658c429036be0d46a6c61b05eb5581e3
饶驹 <raoju1580@qq.com>
2023-01-11 16:09:46 +0800

    :construction_worker: ci(changeset): 测试

--- packages/publish-flow/dist/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// src/index.ts
-var a = 3;
-var b = 3;
-console.log(a + b);
-//# sourceMappingURL=out.js.map
-//# sourceMappingURL=index.js.map
--- packages/publish-flow/dist/index.mjs
+++
@@ -1,6 +0,0 @@
-// src/index.ts
-var a = 3;
-var b = 3;
-console.log(a + b);
-//# sourceMappingURL=out.js.map
-//# sourceMappingURL=index.mjs.map
